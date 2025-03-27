008a70ccb2d7425ec4dfd932052b745dd1d73abd
cheese <deanlemans@mailbox.org>
2025-03-27 22:41:02 +0100

    increase padding between files and folders.
    remove the folderpage and tagpage, to avoid messing the explorer up and
    to declutten(maby in future i can turn them back on when i have to much
    files to sort)

--- quartz.config.ts
+++ quartz.config.ts
@@ -79,10 +79,10 @@ const config: QuartzConfig = {
     filters: [Plugin.RemoveDrafts()],
     emitters: [
       Plugin.AliasRedirects(),
-      Plugin.ComponentResources(),
+      //Plugin.ComponentResources(),
       Plugin.ContentPage(),
-      Plugin.FolderPage(),
-      Plugin.TagPage(),
+      //Plugin.FolderPage(),
+      //Plugin.TagPage(),
       Plugin.ContentIndex({
         enableSiteMap: true,
         enableRSS: false,
@@ -91,7 +91,7 @@ const config: QuartzConfig = {
       Plugin.Static(),
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
-      Plugin.CustomOgImages(),
+      //Plugin.CustomOgImages(),
     ],
   },
 }
