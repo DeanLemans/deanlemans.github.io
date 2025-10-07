187cff1eef7d83c8516f5938c7f202974f994e28
Dean Henny Anton Lemans Haar <deanlemans@mailbox.org>
2025-10-07 02:18:47 +0100

    fix explorer exclude

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -55,7 +55,7 @@ export const defaultContentPageLayout: PageLayout = {
     }),
     Component.Explorer({
       filterFn: (node) => {
-        const omit = ["00-captures", "journal"] // to exclude specified nodes
+        const omit = ["00-raw", "journal", "content/02-resources/02_03-clippings"] // to exclude specified nodes
         return !omit.includes(node.displayName.toLowerCase())
       },
     }),
