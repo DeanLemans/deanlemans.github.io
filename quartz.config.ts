fd5fb547338563f4afd52b92f239f4f7285156a5
Dean Henny Anton Lemans Haar <deanlemans@mailbox.org>
2025-11-04 20:29:29 +0100

    removed till i fix markup

--- quartz.config.ts
+++ quartz.config.ts
@@ -98,7 +98,7 @@ const config: QuartzConfig = {
       Plugin.Static(),
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
-      Plugin.CustomOgImages(), // Comment speed up build time
+      //Plugin.CustomOgImages(), // Comment speed up build time
     ],
   },
 }
