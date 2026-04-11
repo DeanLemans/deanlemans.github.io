4fbafde25f0ea744a72f64d2bec171a66ba21e01
Dean Henny Anton Lemans Haar <deanlemans@mailbox.org>
2026-04-11 23:07:30 +0200

    fix 'exclude' tag functionality

--- quartz.ts
+++ quartz.ts
@@ -1,5 +1,13 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import { Explorer } from "@quartz-community/explorer"
 
 const config = await loadQuartzConfig()
 export default config
 export const layout = await loadQuartzLayout()
+
+Explorer({
+  filterFn: (node) => {
+    // exclude files with the tag "exclude"
+    return node.data?.tags?.includes("exclude") !== true
+  },
+})
