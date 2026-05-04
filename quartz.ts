bb9b0afd866f6dcce29d205b6cf31805d1f00da1
Dean Henny Anton Lemans Haar <deanlemans@mailbox.org>
2026-05-04 22:46:03 +0200

    not needed
    remove custom exclude tag workaround, upstream added better tag

--- quartz.ts
+++ quartz.ts
@@ -1,13 +1,6 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
-import { Explorer } from "@quartz-community/explorer"
+import * as ExternalPlugin from "./.quartz/plugins"
 
 const config = await loadQuartzConfig()
 export default config
 export const layout = await loadQuartzLayout()
-
-Explorer({
-  filterFn: (node) => {
-    // exclude files with the tag "exclude"
-    return node.data?.tags?.includes("exclude") !== true
-  },
-})
