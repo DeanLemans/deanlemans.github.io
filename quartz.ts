8ffaa186384d09e4404d9066ac2427b7a33a7a5a
Dean Henny Anton Lemans Haar <deanlemans@mailbox.org>
2026-07-29 13:31:04 +0200

    new site config with _exclude

--- quartz.ts
+++ quartz.ts
@@ -1,6 +1,12 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 import * as ExternalPlugin from "./.quartz/plugins"
 
+ExternalPlugin.Explorer({
+  filterFn: (node) => {
+    return node.data?.tags?.includes("_excluded") !== true
+  },
+})
+
 const config = await loadQuartzConfig()
 export default config
 export const layout = await loadQuartzLayout()
